05792be1cb115349e35d74d8097c9caf1413ed81
Zhiheng Wang <zhihengw@gmail.com>
2023-06-13 22:13:58 +0800

    Fix several issue in the dev driver.

--- run_lingmind_api_server_dev.sh
+++ run_lingmind_api_server_dev.sh
@@ -13,12 +13,12 @@ sleep 5
 worker_name0="llm01-6b-gov"
 worker_port0=22002
 echo "Starting worker ${worker_name0}..."
-nohup CUDA_VISIBLE_DEVICES=6 python3 -m fastchat.serve.model_worker \
+CUDA_VISIBLE_DEVICES=6 nohup python3 -m fastchat.serve.model_worker \
 	--model-name "${worker_name0}" \
-	--model-path /home/zhihengw/model/chatglm-6b-zhongke-ft
+	--model-path /home/zhihengw/model/chatglm-6b-zhongke-ft \
 	--port ${worker_port0} \
 	--controller-address http://localhost:${controller_port} \
-	--worker-address http://localhost:${worker_port0} >${worker_name0}.nohup. 2>&1 &
+	--worker-address http://localhost:${worker_port0} >${worker_name0}.nohup 2>&1 &
 echo "$!" > ${worker_name0}.pid
 sleep 1
 
@@ -26,7 +26,7 @@ sleep 1
 worker_name1="llm01-6b"
 worker_port1=22003
 echo "Starting worker ${worker_name1}..."
-nohup CUDA_VISIBLE_DEVICES=6 python3 -m fastchat.serve.model_worker \
+CUDA_VISIBLE_DEVICES=6 nohup python3 -m fastchat.serve.model_worker \
 	--model-name "${worker_name1}" \
 	--model-path /home/zhihengw/model/chatglm-6b \
 	--port $worker_port1 \
@@ -39,7 +39,7 @@ sleep 1
 worker_name2="llm02-13b-gov"
 worker_port2=22004
 echo "Starting worker ${worker_name2}..."
-nohup CUDA_VISIBLE_DEVICES=7 python3 -m fastchat.serve.model_worker \
+CUDA_VISIBLE_DEVICES=7 nohup python3 -m fastchat.serve.model_worker \
 	--model-name "${worker_name2}" \
 	--model-path /home/zhihengw/model/BELLE-LLaMA-EXT-13B-zhongke-lora \
 	--port ${worker_port2} \
@@ -51,7 +51,7 @@ sleep 1
 # API server
 openai_api_server_port=9318
 nohup python3 -m fastchat.serve.openai_api_server --host ${local_ip_address} \
-	                                                --controller-address http://localhost:${controller_port} \
+	                                          --controller-address http://localhost:${controller_port} \
                                                   --port ${openai_api_server_port} >nohup.openai_api_server 2>&1 &
 echo "$!" > openai_api.pid
 sleep 1
@@ -59,7 +59,7 @@ sleep 1
 # LingMind server
 lingmind_api_server_port=9317
 nohup python3 -m lingmind.lingmind_api_server --port ${lingmind_api_server_port} \
-                                              --host ${openai_api_server_port} \
-	                                            --controller-address http://localhost:${controller_port} \
+                                              --host ${local_ip_address} \
+	                                      --controller-address http://localhost:${controller_port} \
                                               --use-auto-agent >nohup.lingmind_api_server 2>&1 &
 echo "$!" > lingmind_api.pid
