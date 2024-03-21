da20d19e0c4c323ae9a033205036203922b7da3a
zhihengw <zhihengw@gmail.com>
2024-03-21 23:56:45 +0800

    test

--- run_shenmo_api_server.sh
+++ run_shenmo_api_server.sh
@@ -121,11 +121,12 @@ function start_server {
   worker_name2="qwen-14b-q8"
   echo "Starting worker ${worker_name2}..."
 	#  --model-path /home/models/Qwen-14B-Chat-Int8 \
+	#  --model-path /home/models/Qwen1.5-14B-Chat-GPTQ-Int8 \
   #CUDA_VISIBLE_DEVICES=$GPU0,$GPU1,$GPU2,$GPU3 nohup python3 -m fastchat.serve.model_worker \
   CUDA_VISIBLE_DEVICES=$GPU1,$GPU2 nohup python3 -m ${model_worker} \
     --num-gpus 2 \
 	  --model-name "${worker_name2}" \
-	  --model-path /home/models/Qwen1.5-14B-Chat-GPTQ-Int8 \
+	  --model-path /home/models/Qwen1.5-7B-Chat-AWQ \
 	  --port $worker_port2 \
 	  --controller-address ${controller_address} \
 	  --worker-address http://localhost:${worker_port2} >${LOG_DIR}/${worker_name2}.nohup 2>&1 &
